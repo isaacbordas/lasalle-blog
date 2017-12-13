d0d21d7d8851349d5fdc9008742011eda85b6ae9
Isaac <isaac.bordas@gmail.com>
2017-12-13 16:02:04 +0100

    sql & index

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Blog\Page;
+
+$page1 = new Page();
---
+++ src/Page.php
@@ -0,0 +1,9 @@
+<?php
+namespace Blog;
+
+class Page {
+    public function __construct()
+    {
+        echo "hello, i am a page.";
+    }
+}
