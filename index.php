bd128b5df96728f5178c07943db587a1596d365a
isaacbordas-lasalle <isaac.bordas.2017@salleurl.edu>
2017-12-14 19:16:48 +0100

    Move Twig init

--- index.php
+++ index.php
@@ -7,7 +7,5 @@ use Model\Article;
 $article = new Article();
 $all = $article->getAll();
 
-$loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader);
 $template = $twig->loadTemplate('index.html.twg');
 echo $template->render(array('articles' => $all, 'page_title' => 'Inicio: Listado de artículos'));
--- init.php
+++ init.php
@@ -2,6 +2,9 @@
 include_once ('config.php');
 include_once ('vendor/autoload.php');
 
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
 if (debug === 1) {
     error_reporting(E_ALL | E_STRICT);
     ini_set("display_errors", 1);
