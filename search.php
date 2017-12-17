48017e6bc5b9ada239f589688ef56db73d321fa6
isaacbordas-lasalle <isaac.bordas.2017@salleurl.edu>
2017-12-17 10:50:59 +0100

    Added date to viewArticle

--- search.php
+++ search.php
@@ -4,19 +4,18 @@ require "init.php";
 
 use Model\Articles;
 
-$articleId = filter_input(INPUT_GET, 'articleid', FILTER_SANITIZE_NUMBER_FLOAT);
-$articleId = (!empty($articleId) ? $articleId : null);
+if (empty($_GET['searchby']) || empty($_GET['keyword']) {
+    header('Location: index.php?error=3');
+    die();
+}
+
+$keyword = filter_input(INPUT_GET, 'keyword', FILTER_SANITIZE_STRING);
 
 $article = new Articles();
-$oneArticle = $article->getById($articleId);
+$searchArticle = $article->search($articleId);
 
 $tag = explode(',', $oneArticle[0]->tags);
 $tags = array_map('trim', $tag);
-
-if (empty($oneArticle)) {
-    header('Location: 404.php');
-    die();
-}
 
 $template = $twig->loadTemplate('viewArticle.html.twg');
 echo $template->render(array('articles' => $oneArticle, 'page_title' => $oneArticle[0]->title, 'etiquetas' => $tags));
--- src/Articles.php
+++ src/Articles.php
@@ -8,7 +8,7 @@ class Articles extends Article
     public function getAll()
     {
         $db = new Db();
-        $db->query('SELECT id, title, author, content, tags, created_at FROM articles');
+        $db->query('SELECT id, title, content FROM articles');
         $row = $db->resultset(__CLASS__);
 
         if ($db->rowCount() > 0){
@@ -21,7 +21,7 @@ class Articles extends Article
     public function getById($id)
     {
         $db = new Db();
-        $db->query('SELECT id, title, author, content, tags, created_at FROM articles WHERE id = :articleId');
+        $db->query('SELECT title, author, content, tags, created_at FROM articles WHERE id = :articleId');
         $params = array(
                     array('var' => ':articleId', 'value' => $id, 'type' => \PDO::PARAM_INT)
                 );
@@ -73,6 +73,24 @@ class Articles extends Article
 
         if ($db->rowCount() > 0){
             return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function search()
+    {
+        $db = new Db();
+        $db->query('SELECT id, title, author, content, tags, created_at FROM articles WHERE id = :articleId');
+        $params = array(
+                    array('var' => ':articleId', 'value' => $id, 'type' => \PDO::PARAM_INT)
+                );
+        $db->bind($params);
+
+        $row = $db->resultset(__CLASS__);
+
+        if ($db->rowCount() > 0){
+            return $row;
         }else{
             return false;
         }
